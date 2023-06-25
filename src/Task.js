9b5f9573caedfb6acf567c0ad9963ef9489115fd
Irwinsyah Maulana Putra <79708595+irwnsyhmp@users.noreply.github.com>
2023-06-25 09:29:27 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Irwinsyah Maulana Putra" // TODO: replace this
+const studentId = "FE5187660" // TODO: replace this
+
+export { url, studentName, studentId }
